b3e4488218cc1307247710f804041b4b3f793236
ArthurYaxiongWangRRC <yaxiongwang@rrc.ca>
2024-11-20 21:11:08 -0600

    Fetching data with useEffect

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import { useState, useEffect } from "react";
+
+const url = "https://geek.itheima.net/v1_0/channels";
+
 function App() {
-  return <div>Be proficient at React</div>;
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((jsonList) => setData(jsonList.data.channels));
+  }, []);
+
+  return (
+    <div>
+      <ul>
+        {data.map((item) => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
